51d9f51470d18f84337d8b6f57b48e34c2f0ea50
Maxwell Alexander <alexander.maxwell84@gmail.com>
2022-09-06 16:32:29 -0500

    Commit inicial. Se crea el proyecto

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header ">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="bg-red-600">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
